92acccf8a0dd86df332314023caa1111c44461b4
sergiovergaraehu <sergiovergaraehu@gmail.com>
2020-02-13 14:49:17 +0100

    tipos de strings

--- Server/DataType.cs
+++ Server/DataType.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 namespace adm
 {
-	string varchar;
-	//string  integerTipo, stringTipo, doubleTipo;
 	public class DataType
 	{
-		public DataType()
-		{
-			
-		}
+		char varcharDBType;
+		string stringTipo, stringTipoUnknown, stringValueOfData;
+		int integerDBType;
+		double doubleType;
+		DateTime dateTimeType;
+		string[] typeListOptions = new string[] { "ADD", "ADD CONSTRAINT", "ALTER", "ALTER COLUMN", "ALTER TABLE", "ALL", "AND", "ANY", "AS", "ASC", "BACKUP DATABASE",
+			"BETWEEN", "CASE", "CHECK", "COLUMN", "CONSTRAINT", "CREATE", "CREATE DATABASE", "CREATE INDEX", "CREATE OR REPLACE VIEW","CREATE TABLE", "CREATE PROCEDURE",
+			"CREATE UNIQUE INDEX", "CREATE VIEW", "DATABASE", "DEFAULT", "DELETE", "DESC", "DISTINCT","DROP", "DROP COLUMN", "DROP CONSTRAINT", "DROP DATABASE", "DROP DEFAULT",
+			"DROP INDEX", "DROP TABLE", "DROP VIEW", "EXEC","EXISTS", "FOREIGN KEY", "FROM","FULL OUTER JOIN", "GROUP BY", "HAVING", "IN", "INDEX", "INNER",
+			"INNER JOIN", "INNER INTO", "INSERT INTO SELECT", "IS NULL", "JOIN", "LEFT JOIN","LIKE", "LIMIT", "NOT","NOT NULL", "OR", "ORDER BY", "OUTER JOIN", "PRIMARY KEY", "PROCEDURE", "RIGHT JOIN","ROWNUM", "SELECT","SELECT DISTINCT", "SELECT INTO", "SELECT TOP", "SET", "TABLE", "TOP", "TRUNCATE TABLE", "UNION","UNION ALL", "UNIQUE", "UPDATE","VALUES","VIEW", "WHERE"}}
+
+
+	public DataType(string stringPiece)
+	{
+		//stringTipoUnknown = stringPiece;
 	}
+
+
+
+
+
+
+
+
+}
 }
