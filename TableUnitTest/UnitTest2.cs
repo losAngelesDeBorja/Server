df658283b60f210424cc1e146a83e4f6005abb26
sergiovergaraehu <sergiovergaraehu@gmail.com>
2020-02-20 13:15:17 +0100

    Removed the code of test after Test finalised

--- TableUnitTest/UnitTest2.cs
+++ TableUnitTest/UnitTest2.cs
@@ -9,19 +9,7 @@ namespace TableUnitTest
         [TestMethod]
         public void AddAndTestData()
         {
-            //create table
-            adm.Table table = new adm.Table("MyTable", 10);
-            //Create Atribute
-            string name = "Name";
-            adm.DataType newTipo = new adm.DataType();
-    
-            table.addAttribute(name, newTipo);
-            adm.Database newDB = new adm.Database("miDB", "user", "user");
-            //The SQL senetence will arrive from client when the programming finnished.
-            // Until the SQL request is written into a text file (XML format?), it is introduced as string here: 
-            string sql = "CREATE TABLE Persons (PersonID int,LastName varchar(255),FirstName varchar(255),Address varchar(255),City varchar(255) );";
-            newDB.createDatabaseByText(sql);
-
+        
 
         }
         [TestMethod]
