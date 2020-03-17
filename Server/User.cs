f18a07f17a8f42743b8bef2e612f9e9134eeca59
Daniel Esteban <danielesteban1921@gmail.com>
2020-03-17 13:27:12 +0100

    created class user

---
+++ Server/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace adm
+{
+    class User
+    {
+        public string name;
+        public string password;
+        List privileges;
+
+        public User(string pName, string pPassword)
+        {
+            pName = name;
+            pPassword = password;
+        }
+
+        public void setPrivilege(string name, string privilege)
+        {
+
+        }
+
+        public Boolean getPrivilege(string name)
+        {
+            return false;
+        }
+    }
+}
