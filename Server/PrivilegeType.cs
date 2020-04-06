1946383f95486728746b7bfa6d8693d87ec37934
dapro23 <dramos97.dr24@gmail.com>
2020-04-06 22:29:12 +0200

    #19

--- Server/PrivilegeType.cs
+++ Server/PrivilegeType.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Server
+namespace adm
 {
     public enum PrivilegeType { DELETE, INSERT, SELECT, UPDATE };
     // d
