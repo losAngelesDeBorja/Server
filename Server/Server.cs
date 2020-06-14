6becd86bc927472a865b488cde4124efa8696285
David Ramos <dramos97.dr24@gmail.com>
2020-06-14 21:55:27 +0200

    #28 Both the client and the server connect, but due to the great modification of the code that this implies, only the point of connection is reached.

--- Server/Server.cs
+++ Server/Server.cs
@@ -9,6 +9,9 @@ using System.Data;
 using System.Threading.Tasks;
 using System.Diagnostics;
 
+using System.Text.RegularExpressions;
+
+
 namespace adm
 {
     class Server
@@ -23,8 +26,8 @@ namespace adm
             dbList = new List<Database>();
         }
 
-
-
+        
+        
 
         // Create a new user
         static bool MakeNewUser(string userInfo)
@@ -374,8 +377,82 @@ namespace adm
         }
         
         
+
         private static void Main()
         {
+            TcpListener server = null;
+            try
+            {
+
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                // Set the TcpListener on port 8001.
+                server = new TcpListener(localAddr, 8001);
+
+
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = "";
+                String response = "";
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also use server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+                    
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+
+                        byte[] msg = Encoding.ASCII.GetBytes(response);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", response);
+                    }
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+
+
+
+            /*
+                        
+                    
+            
             Console.Clear();
             Server server = new Server();
             Database dbInit=server.initializeDbEngine();
@@ -515,7 +592,16 @@ namespace adm
                 client.Close();
                 listener.Stop();
             }
+        
+            */
+
         }
+
+
+
+
+
+
     }
 }
 
